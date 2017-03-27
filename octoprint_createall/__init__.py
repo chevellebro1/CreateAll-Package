8de96a94390a9f3a2c53c27b033b10a4d5dd0fcf
Richard Hardman <chevellebro1@gmail.com>
2017-03-26 22:21:19 -0400

    upload

---
+++ octoprint_createall/__init__.py
@@ -0,0 +1,81 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+import flask
+
+
+class V8themePlugin(octoprint.plugin.SettingsPlugin,
+                    octoprint.plugin.TemplatePlugin,
+                    octoprint.plugin.AssetPlugin,
+                    octoprint.plugin.SimpleApiPlugin,
+                    octoprint.plugin.StartupPlugin):
+
+    printer_name = ""
+
+    def get_settings_defaults(self):
+        return dict(
+            webcam=dict(
+                enabled=False
+                )
+        )
+
+    def get_assets(self):
+        return dict(
+            js=['js/v8theme.js'],
+            css=['css/main.css'],
+        )
+
+    def get_api_commands(self):
+        return dict(
+            update_printer_name=[]
+        )
+
+    def on_api_command(self, command, data):
+        if command == "update_printer_name":
+            self.printer_name = data.get("printer_name")
+            self._plugin_manager.send_plugin_message(
+                self._identifier, dict(printer_name=self.printer_name))
+
+    def on_api_get(self, request):
+        return flask.jsonify(printer_name=self.printer_name)
+
+    # Handles turning off pneumatic devices when print is paused or cancelled
+    # Returns in the form (prefix, postfix)
+    def scripts_hook(self, comm, script_type, script_name, *args, **kwargs):
+        if (not script_type == "gcode" or
+           script_name not in ["afterPrintCancelled", "afterPrintPaused"]):
+            return None
+
+        return ["M42 P2 S0", "M42 P75 S0"], None
+
+    def get_update_information(*args, **kwargs):
+        return dict(
+            v8theme=dict(
+                type="github_commit",
+                user="Voxel8",
+                repo="OctoPrint-V8theme",
+                branch='master',
+                pip="https://github.com/Voxel8/OctoPrint-V8theme/archive/"
+                    "{target_version}.zip",
+            )
+        )
+
+    def get_template_configs(self):
+        return [
+            dict(type="settings", name="Voxel8",
+                 data_bind="visible: loginState.isAdmin()"),
+        ]
+
+
+def __plugin_load__():
+    global __plugin_implementation__
+    global __plugin_hooks__
+    __plugin_implementation__ = V8themePlugin()
+
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config":
+            __plugin_implementation__.get_update_information,
+        "octoprint.comm.protocol.scripts":
+            __plugin_implementation__.scripts_hook,
+    }
